47b48e4e606557dea6b22d6257b03a91a0e0193a
simoncrabtree <simon@crabsticks.net>
2016-04-14 16:39:24 +0100

    Further tests

--- client/src/app/reducers/team-spec.js
+++ client/src/app/reducers/team-spec.js
@@ -2,22 +2,38 @@ import { expect } from 'chai'
 import { reducerReduce } from '../specHelper'
 import team from './team'
 import {
+  LOGIN_REQUEST,
   LOGIN_SUCCESS
 } from '../actions'
 
 describe('team', () => {
   it('is not logged in on startup', () => {
     const state = reducerReduce(team, [])
+    expect(state.isLoggingIn).to.be.false
     expect(state.isLoggedIn).to.be.false
   })
 
-  describe('user logs in', () => {
+  describe('user attempts to log in', () => {
+    const state = reducerReduce(team, [{
+      type: LOGIN_REQUEST
+    }])
+
     it('is logged in', () => {
-      const state = reducerReduce(team, [{
-        type: LOGIN_SUCCESS,
-        id: 'token-123'
-      }])
+      expect(state.isLoggingIn).to.be.true
+    })
+  })
+
+  describe('user logs in and successful response is received', () => {
+    const state = reducerReduce(team, [{
+      type: LOGIN_REQUEST
+    }, {
+      type: LOGIN_SUCCESS,
+      id: 'token-123'
+    }])
+
+    it('is logged in', () => {
       expect(state.isLoggedIn).to.be.true
+      expect(state.isLoggingIn).to.be.false
     })
   })
 })
